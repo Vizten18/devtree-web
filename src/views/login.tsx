038ed53fdeea8d2d28ca13cfc79c9b4139cdd606
vizten18 <vizten18@hotmail.com>
2026-06-17 19:54:42 -0400

    Add social media icons and AuthLayout component

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <div>Login View</div>
       <nav>
         <Link to="/auth/register">Go to Register</Link>
       </nav>
